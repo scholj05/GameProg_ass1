da2896843571a4d78e42cd254a9d2ae1b4be0eea
· Aquila · <apocalaeon@hotmail.com>
2017-04-26 13:07:23 +1200

    Added basic editor class + pugixml support

---
+++ take2/Editor.cpp
@@ -0,0 +1,13 @@
+#include "Editor.h"
+
+void Editor::editor() {
+	// get a test document
+	pugi::xml_document doc;
+	doc.load_string("<foo bar='baz'>hey</foo>");
+
+	// tag::code[]
+	// save document to file
+	std::cout << "Saving result: " << doc.save_file("save_file_output.xml") << std::endl;
+	// end::code[]
+
+}
---
+++ take2/Editor.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+#include "Level.h"
+#include "MyShape.h"
+#include "physics.h"
+#include "..\resources\pugixml-1.8\src\pugixml.hpp"
+
+class Editor{
+	private:
+
+
+	public:
+		void Editor::editor();
+
+
+	};
+
