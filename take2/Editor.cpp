e94da3f26cdca2816fef5f38804cda58f0d396f3
· Aquila · <apocalaeon@hotmail.com>
2017-04-27 19:51:52 +1200

    made a start on parameters class to store string data to go into xml

--- take2/Editor.cpp
+++ take2/Editor.cpp
@@ -10,7 +10,7 @@ void Editor::save(b2Body* bodyList) {
 
 	// get a test document
 	pugi::xml_document doc;
-	doc.load_string("<mainelement><subelement>derp</subelement></mainelement>");
+	doc.load_string("<root><subelement>derp</subelement></root>");
 	while (bodyList != NULL)
 	{
 
@@ -29,8 +29,46 @@ void Editor::load() {
 
 	pugi::xml_parse_result result = doc.load_file("save_file.xml");
 
-	pugi::xml_node subelementnode = doc.child("mainelement").child("subelement");
+	pugi::xml_node subelementnode = doc.child("root").child("subelement");
 
-	std::cout << "Load result: " << doc.child("mainelement").child("subelement").child_value() << std::endl;
+	std::cout << "Load result: " << doc.child("root").child("subelement").child_value() << std::endl;
 	// end::code[]
 }
+
+Parameters::Parameters() {
+	std::string m_b2BodyDef = "";
+	std::string m_b2BodyDefType = "";
+	std::string m_calculateRectangle = "";
+	std::string m_recPosition = "";
+	std::string m_recWidth = "";
+	std::string m_recHeight = "";
+	std::string m_rotation = "";
+	std::string m_b2FixtureDef = "";
+	std::string m_density = "";
+	std::string m_friction = "";
+	std::string m_restitution = "";
+}
+
+Parameters::Parameters(std::string b2bodydef, std::string b2bodydeftype, std::string calcrec, std::string recpos,
+	std::string recwid, std::string rechei, std::string rot, std::string fixdef, std::string density, std::string friction, std::string restitution) {
+	std::string m_b2BodyDef = b2bodydef;
+	std::string m_b2BodyDefType = b2bodydeftype;
+	std::string m_calculateRectangle = calcrec;
+	std::string m_recPosition = recpos;
+	std::string m_recWidth = recwid;
+	std::string m_recHeight = rechei;
+	std::string m_rotation = rot;
+	std::string m_b2FixtureDef = fixdef;
+	std::string m_density = density;
+	std::string m_friction = friction;
+	std::string m_restitution = restitution;
+}
+
+std::string Parameters::getb2BodyDef() {
+	return m_b2BodyDef;
+}
+
+void Parameters::setb2BodyDef(std::string b2BodyDef) {
+	m_b2BodyDef = b2BodyDef;
+}
+
--- take2/Editor.h
+++ take2/Editor.h
@@ -23,5 +23,28 @@ class Editor{
 		void save(b2Body* bodyList);
 		void load();
 
-	};
+};
 
+class Parameters {
+	private:
+		std::string m_b2BodyDef;
+		std::string m_b2BodyDefType;
+		std::string m_calculateRectangle;
+		std::string m_recPosition;
+		std::string m_recWidth;
+		std::string m_recHeight;
+		std::string m_rotation;
+		std::string m_b2FixtureDef;
+		std::string m_density;
+		std::string m_friction;
+		std::string m_restitution;
+
+
+	public:
+
+		Parameters::Parameters();
+		Parameters::Parameters(std::string b2bodydef, std::string b2bodydeftype, std::string calcrec, std::string recpos,
+			std::string recwid, std::string rechei, std::string rot, std::string fixdef, std::string density, std::string friction, std::string restitution);
+		std::string getb2BodyDef();
+		void setb2BodyDef(std::string b2BodyDef);
+};
